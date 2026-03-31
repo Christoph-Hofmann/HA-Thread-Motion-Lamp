de1826f8f18f79e94ced99fd59266e36549e161f
Christoph Hofmann <ch@its-hofmann.de>
2026-03-31 22:20:42 +0200

    update v0.0.44

--- custom_components/matter_motion_lamp/button.py
+++ custom_components/matter_motion_lamp/button.py
@@ -5,8 +5,10 @@ import logging
 from homeassistant.components.button import ButtonEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
+from .const import DOMAIN
 from .updater import async_fetch_updates
 
 _LOGGER = logging.getLogger(__name__)
@@ -15,7 +17,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    async_add_entities([FetchUpdatesButton(hass)])
+    async_add_entities([FetchUpdatesButton(hass, entry)])
 
 
 class FetchUpdatesButton(ButtonEntity):
@@ -25,8 +27,13 @@ class FetchUpdatesButton(ButtonEntity):
     _attr_name = "Fetch Updates"
     _attr_icon = "mdi:cloud-download"
 
-    def __init__(self, hass: HomeAssistant) -> None:
+    def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
         self._hass = hass
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, entry.entry_id)},
+            name="Matter Motion Lamp",
+            manufacturer="Christoph-Hofmann",
+        )
 
     async def async_press(self) -> None:
         _LOGGER.info("Fetch Updates button pressed")
